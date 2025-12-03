f5101ca9414d132f9883032fbdc34dc53c3b54f5
Teodora Zhelyazkova <teodora.s.zhelyazkova@gmail.com>
2025-12-03 13:23:24 +0200

    fix formatting

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
-  type                                                          = string
+  type        = string
 }
 
 variable "resource_group_location" {
